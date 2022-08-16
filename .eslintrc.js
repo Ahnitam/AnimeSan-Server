238e6ab582663c18f2af1a7f3504eaf2a25b8345
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-08-16 13:05:18 -0300

    commit inicial

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	"env": {
+		"es2021": true,
+		"node": true
+	},
+	"extends": [
+		"plugin:@typescript-eslint/recommended",
+		"eslint:recommended"
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
